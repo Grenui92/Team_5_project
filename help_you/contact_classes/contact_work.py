31196b1b03de59c9c79338f99309aa1a75ef6382
Grenui92 <grenui92@gmail.com>
2022-12-27 01:39:00 +0200

    contact_work ready

--- help_you/contact_classes/contact_work.py
+++ help_you/contact_classes/contact_work.py
@@ -20,7 +20,6 @@ class WorkContact:
         except FileNotFoundError:
             print(self.contacts_book.save_to_file())
 
-    """Методи приймають розпарсені дані від користувача args"""
 
     def save_to_file(self):
         return self.contacts_book.save_to_file()
@@ -28,12 +27,12 @@ class WorkContact:
     def load_from_file(self):
         return self.contacts_book.load_from_file()
 
-    def create(self, *args) -> str:
-        if args[0] in self.contacts_book:
-            return f"Note with name '{args[0]} already exist."
+    def create(self, name, *_) -> str:
+        if name in self.contacts_book:
+            return f"Note with name '{name} already exist."
         else:
-            self.contacts_book[args[0]] = Record(args[0])
-            return f"Contact with name {args[0]} successfully created."
+            self.contacts_book[name] = Record(name)
+            return f"Contact with name {name} successfully created."
 
     def show_all(self, *_) -> list:
         """Створює рядок з інформацією про кожен контакт"""
@@ -45,14 +44,13 @@ class WorkContact:
 
         return rec_info
 
-    def show_one(self, *args: list) -> str:
-        name = args[0]
+    def show_one(self, name, *_) -> str:
         return f"{str(self.contacts_book[name]) if name in self.contacts_book else f'Contact {name} is not founded'}"
 
-    def show_page(self, *args):
+    def show_page(self, number_of_contacts, *_):
         """Ітеруємось по записам і формуєм рядок з контактами по n штук на сторінку"""
         result = []
-        for page in self.contacts_book.iterator(int(args[0])):
+        for page in self.contacts_book.iterator(int(number_of_contacts)):
             result_str = ""
             for record in page:
                 result_str += str(self.contacts_book[record]) + "\n"
@@ -68,31 +66,28 @@ class WorkContact:
         else:
             return 'Not deleted'
 
-    def delete_one(self, *args: list):
-        name = args[0]
+    def delete_one(self, name, *_):
         if name in self.contacts_book:
             del self.contacts_book[name]
             return f"Contact {name} is deleted"
         else:
             f"Contact {name} is not in book"
 
-    def add_values(self, *args):
+    def add_values(self, name: str, args: list):
         try:
-            name = args[0]
-            field = args[1][0]
-            value = args[1][1:]
+            field = args[0]
+            value = args[1:]
         except IndexError:
             raise IndexError("Not enough information.")
 
-        records_fields_methods = {'phones': self.contacts_book.data[name].add_phone,
-                                  'emails': self.contacts_book.data[name].add_email,
-                                  'addresses': self.contacts_book.data[name].add_address,
-                                  'birthday': self.contacts_book.data[name].set_birthday
+        records_fields_methods = {'phones': self.contacts_book[name].add_phone,
+                                  'emails': self.contacts_book[name].add_email,
+                                  'addresses': self.contacts_book[name].add_address,
+                                  'birthday': self.contacts_book[name].set_birthday
                                   }
         return records_fields_methods[field](" ".join(list(value)))
 
-    def search_in(self, search_data):
-
+    def search_in(self, search_data: str, *_) -> list:
         """Пошук заданого фрагмента у контактах"""
         result = []
         for value in self.contacts_book.values():
@@ -104,20 +99,19 @@ class WorkContact:
                 result.append(value)
         return result
 
-    def edit_information(self, *args):
-        name = args[0]
-        command = args[1][0]
-        field = args[1][1]
-        values = args[1][2:]
+    def edit_information(self, name: str, args: list) -> str:
+        command = args[0]
+        field = args[1]
+        values = args[2:]
 
         if name in self.contacts_book.data:
             return self.contacts_book.data[name].edit_information_contact(command, field, values)
         else:
             return "This contact doesn't exist!"
 
-    def show_nearest_birthdays(self, *args):
+    def show_nearest_birthdays(self, days: str, *_) -> list:
 
-        n = int(args[0])
+        n = int(days)
         n_days_birthday = []
 
         for contact in self.contacts_book.values():
@@ -125,22 +119,20 @@ class WorkContact:
                 n_days_birthday.append(f"Days to {contact.name.value}'s birthday : {contact.days_to_birthday()} days \n")
         return n_days_birthday if n_days_birthday else f'No birthdays in nearest {n} days'
 
-    def days_to_birthday_for_one(self, *args):
-        name = args[0]
+    def days_to_birthday_for_one(self, name: str, *_) -> str:
         return self.contacts_book.data[name].days_to_birthday()
 
-    def days_to_birthday_for_all(self, *_):
-        bdays = []
+    def days_to_birthday_for_all(self, *_) -> list:
+        birthdays = []
+        for contact in self.contacts_book.values():
+            birthdays.append(f"Days to {contact.name.value}'s birthday: {contact.days_to_birthday()}")
 
-        for contact in self.contacts_book.data.values():
-            bdays.append(f"Days to {contact.name.value}'s birthday: {contact.days_to_birthday()}")
+        return birthdays if birthdays else 'There is no birthdays in contacts'
 
-        return bdays if bdays else 'There is no birthdays in contacts'
-
-    def edit_name(self, *args):
-        name = args[0]
-        new_name = args[1]
-        if new_name in self.contacts_book.data:
-            return f"{name}'s name has been changed to {new_name}"
-        else:
-            return f'{name} not in contacts'
+    def edit_name(self, name: str, args: list) -> str:
+        new_name = args[0]
+        record: Record = self.contacts_book[name]
+        record.name.value = new_name
+        self.contacts_book[new_name] = record
+        del self.contacts_book[name]
+        return f"{name}'s name has been changed to {new_name}"
