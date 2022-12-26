79660ab5d2b87f8f096db2ef7210fca78d77fa8c
Grenui92 <grenui92@gmail.com>
2022-12-27 00:27:02 +0200

    new contact_work.py

--- help_you/contact_classes/contact_work.py
+++ help_you/contact_classes/contact_work.py
@@ -1,9 +1,13 @@
+from ..book import Book
+from os import path
+from record import Record
+from fields import Phone, Email, Address, Birthday
 class WorkContact:
     def __init__(self):
         """При ініціалізації відкриваєм бінарний файл з якого
         створюєм нову книжку. Якщо файл пустий - створюєм
         порожню книжку"""
-        self.contacts_book = ContactBook()
+        self.contacts_book = Book(path.join("database", "contacts"))
         try:
             print(self.contacts_book.load_from_file())
         except FileNotFoundError:
@@ -80,11 +84,11 @@ class WorkContact:
                                   }
         return records_fields_methods[field](" ".join(list(value)))
 
-    def search_in(self, *args: list):
+    def search_in(self, search_data):
 
         """Пошук заданого фрагмента у нотатках"""
         result = []
-        for value in self.data.values():
+        for value in self.contacts_book.values():
             if search_data in (value.name.value,
                                *[phone.value for phone in value.phones if isinstance(phone, Phone)],
                                *[email.value for email in value.emails if isinstance(email, Email)],
@@ -130,7 +134,6 @@ class WorkContact:
         name = args[0]
         new_name = args[1]
         if new_name in self.contacts_book.data:
-            self.contacts_book.data[new_name] = self.contacts_book.data.pop[name]
             return f"{name}'s name has been changed to {new_name}"
         else:
             return f'{name} not in contacts'
